b349055a0de5d5d0416f22332b81a7a6e20f8773
BatiEng <alibatikan@gmail.com>
2025-06-10 17:33:31 +0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/~tdomain/",
+  // base: "/~tdomain/",
   plugins: [react(), tailwindcss()],
 });
