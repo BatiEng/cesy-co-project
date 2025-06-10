48964f53b5eb4c438cb9c2ec3ca1ce5162ade7cb
BatiEng <alibatikan@gmail.com>
2025-06-10 17:36:36 +0300

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/~tdomain/">
+    {/* <BrowserRouter basename="/~tdomain/"> */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
